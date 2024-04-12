963c38b5d091245dc19f2a9e682a3650b22ed016
ereism1 <eduardomreis2@gmail.com>
2024-04-12 14:06:45 -0300

    segundo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,69 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
+import  from "src/app/globals.css";
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
-}
+      return (
+        <>
+          <header>
+            <div className="lado">s</div>
+            <div className="meio">
+              <div id="cima">
+                <div id="layout">
+                  LS Lacrei Saude
+                </div>
+                <div id="layout">
+                  <a href="#" >Quem somos</a>
+                  <a href="" >Ajuda</a>
+                  <button>Entrar</button>
+                </div>
+              </div>
+              <div id="cima">f</div>
+              <div id="meio2">
+                <div id="ladoDir">
+                  <div id="cima2">l</div>
+                  <div id="meio4">
+                    <p>Junte-se à nossa comunidade</p>
+                    <p>Encontre atendimento clínico de qualidade <br /> ou entre para o time de profissionais da <br /> Lacrei Saúde.</p>
+                  </div>
+                  <div id="void">l</div>
+                  <div id="void2">
+                    <button id="buscar">Buscar Atendimento</button>
+                    <button id="buscar">Oferecer Atendimento</button>
+                  </div>
+                  <div id="baixo2">l</div>
+                </div>
+                <div id="meio3">
+                  <div id="cima2">l</div>
+                  <div id="meio4">l</div>
+                  <div id="void">l</div>
+                  <div id="void3"></div>
+                  <div id="baixo2">l</div>
+                </div>
+                <div id="ladoEsq"></div>
+              </div>
+              <div id="baixo"></div>
+              <div id="baixofooter">
+                <div id="footer">LS Lacrei Saude</div>
+                <div id="footer">
+                  Lacrei saude<br />quem somos<br />nosso proposito<br />missao, visao e valor<br />acessibilidade
+                </div>
+                <div id="footer">
+                  Saúde<br />buscar atendimento<br />oferecer atendimento
+                </div>
+                <div id="footer">
+                  Segurança e privacidade<br />politica de privacidade<br />termos de uso<br />direitos de titular
+                </div>
+              </div>
+              <div id="C">
+                A lacrei não oferece tratamento médico emergencial. Em caso de emergência, procure o hospital mais próximo.<br />
+                Em caso de problemas psicológicos, ligue para 188 (CVV) ou acesse o site www.cvv.org.br<br />
+                Copyright @2021 Lacrei. todos os direitos reservados. CNPJ: 51.265.351/0001-65
+              </div>
+            </div>
+            <div className="lado">s</div>
+          </header>
+        </>
+      );
+    
+    
+    
+    }
